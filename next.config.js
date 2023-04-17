ea43b140a2a0d61ca2f7a8d3297fb69066483f7f
HenrikCheng <henrikcheng@live.se>
2023-04-17 11:08:37 +0200

    working employee grid

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.1337co.de",
+        port: "",
+        pathname: "/profile/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
